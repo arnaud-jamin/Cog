6a7a3a69894ac3dd48f6d308ad257f68d442e217
Anthony Moniz <mega_dude__@hotmail.com>
2024-11-10 21:02:05 -0500

    fix unused code

--- Plugins/Cog/Source/CogImgui/Private/CogArrayUtility.cpp
+++ Plugins/Cog/Source/CogImgui/Private/CogArrayUtility.cpp
@@ -1,13 +1,1 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "CogArrayUtility.h"
-
-
-CogArrayUtility::CogArrayUtility()
-{
-}
-
-CogArrayUtility::~CogArrayUtility()
-{
-}
